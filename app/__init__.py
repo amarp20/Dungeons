e563a1b82d37f0a12bcd3f1410a6669809f2de67
amarp20 <knigth.moro@gmail.com>
2025-06-03 12:51:54 +0200

    los inicios2

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Importar y registrar rutas
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
---
+++ config.py
@@ -0,0 +1,3 @@
+class Config:
+    DEBUG = True
+    SECRET_KEY = 'dev'  # Puedes cambiarlo por algo más seguro si se requiere
