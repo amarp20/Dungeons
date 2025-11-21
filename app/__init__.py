152074c12d8c81930b1bb5238eb701345ddcc5bb
Alejandro Martín Pérez <knight.moro@gmail.com>
2025-11-21 13:13:00 +0100

    Dungeons4

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,23 @@
-from flask import Flask
-from config import Config
+from flask import Flask, render_template, Blueprint
 
+# Definir blueprint
+main = Blueprint('main', __name__)
+
+# Rutas
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/ficha')
+def ficha():
+    return render_template('ficha.html')
+
+@main.route('/hechizos')
+def hechizos():
+    return render_template('hechizos.html')
+
+# Crear la app principal
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
-
-    # Importar y registrar rutas
-    from .app import main
     app.register_blueprint(main)
-
     return app
